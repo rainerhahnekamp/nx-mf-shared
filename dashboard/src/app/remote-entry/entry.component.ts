a8a32412c2cc2f6fb4094e48a8248afe35515a25
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-09-15 19:45:26 +0200

    add readme

--- dashboard/src/app/remote-entry/entry.component.ts
+++ dashboard/src/app/remote-entry/entry.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {GridstackComponent, GridstackModule} from "gridstack/dist/angular";
 import {GridStackOptions} from "gridstack";
 
 @Component({
   selector: 'test-nx-mf-dashboard-entry',
-  template: `<div><h1>This is the Remote</h1><gridstack [options]="gridOptions" /></div>`,
+  template: `
+    <div><h1>This is the Remote</h1>
+      <gridstack [options]="gridOptions"/>
+    </div>`,
   standalone: true,
   imports: [GridstackModule]
 })
@@ -12,8 +15,9 @@ export class RemoteEntryComponent {
   public gridOptions: GridStackOptions = {
     margin: 5,
     children: [ // or call load()/addWidget() with same data
-      {x:0, y:0, minW:2, content:'Item 1'},
-      {x:1, y:0, content:'Item 2'},
-      {x:0, y:1, content:'Item 3'},
+      {x: 0, y: 0, minW: 2, content: 'Item 1'},
+      {x: 1, y: 0, content: 'Item 2'},
+      {x: 0, y: 1, content: 'Item 3'},
     ]
-  }}
+  }
+}
