31849a19907528751d2b95475858079517a5d1f2
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-09-15 19:39:58 +0200

    add gridstack with warning

---
+++ dashboard/src/app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'test-nx-mf-root',
+  template: '<router-outlet></router-outlet>',
+})
+export class AppComponent {}
---
+++ dashboard/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import {GridstackComponent, GridstackModule} from "gridstack/dist/angular";
+import {GridStackOptions} from "gridstack";
+
+@Component({
+  selector: 'test-nx-mf-dashboard-entry',
+  template: `<div><h1>This is the Remote</h1><gridstack [options]="gridOptions" /></div>`,
+  standalone: true,
+  imports: [GridstackModule]
+})
+export class RemoteEntryComponent {
+  public gridOptions: GridStackOptions = {
+    margin: 5,
+    children: [ // or call load()/addWidget() with same data
+      {x:0, y:0, minW:2, content:'Item 1'},
+      {x:1, y:0, content:'Item 2'},
+      {x:0, y:1, content:'Item 3'},
+    ]
+  }}
